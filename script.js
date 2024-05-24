24c0d60cee5c8e59e269a9e1635d706e1ebcd8b9
vinicarrasco <162223456+vinicarrasco@users.noreply.github.com>
2024-05-23 22:29:46 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta1 = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+
+    },
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+
+    }
+];
